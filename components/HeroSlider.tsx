2af582a41b4b0541f4ade5f2d6547f1942d08d31
abdallah <eldonronaldo97@gmail.com>
2026-04-25 10:25:46 +0300

    square add to cart button + updated hero banners

--- components/HeroSlider.tsx
+++ components/HeroSlider.tsx
@@ -2,44 +2,42 @@
 
 import { useEffect, useState } from "react";
 
-const images = [
-  "/banner1.jpg",
-  "/banner2.jpg",
-  "/banner3.jpg",
-  "/banner4.jpg",
-];
-
 export default function HeroSlider() {
+  const images = [
+    "/banner1.jpg",
+    "/banner2.jpg",
+    "/banner3.jpg",
+    "/banner4.jpg",
+  ];
+
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setIndex((prev) => (prev + 1) % images.length);
-    }, 2000); // ⏱ كل 2 ثانية
+    }, 3000); // مدة العرض
 
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <div
-      style={{
-  width: "100%",
-  height: "40vh",
-  minHeight: "260px",
-  maxHeight: "420px",
-  overflow: "hidden",
-  position: "relative",
-}}
-    >
-      <img
-        src={images[index]}
-        style={{
-          width: "100%",
-          height: "100%",
-          objectFit: "cover",
-          transition: "0.5s",
-        }}
-      />
+    <div style={{ position: "relative", height: "600px", overflow: "hidden" }}>
+      {images.map((img, i) => (
+        <img
+          key={i}
+          src={img}
+          style={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            top: 0,
+            left: 0,
+            opacity: i === index ? 1 : 0,
+            transition: "opacity 1s ease-in-out",
+          }}
+        />
+      ))}
     </div>
   );
 }
