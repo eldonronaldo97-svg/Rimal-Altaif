d99c8f611ed44678d9dd2a60b1aee825f577f726
abdallah <eldonronaldo97@gmail.com>
2026-06-07 14:28:07 +0300

    add cart counter

--- components/MobileBottomBar.tsx
+++ components/MobileBottomBar.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import Link from "next/link";
+import { useCart } from "../lib/store";
 
 export default function MobileBottomBar() {
+  const cart = useCart((s) => s.cart);
+
   return (
     <div className="mobile-bar">
       <Link href="/" className="mobile-item">
@@ -15,7 +18,33 @@ export default function MobileBottomBar() {
         <span>Brands</span>
       </Link>
 
-      <Link href="/cart" className="mobile-item">
+      <Link
+        href="/cart"
+        className="mobile-item"
+        style={{ position: "relative" }}
+      >
+        {cart.length > 0 && (
+          <div
+            style={{
+              position: "absolute",
+              top: -8,
+              right: -8,
+              width: 18,
+              height: 18,
+              borderRadius: "50%",
+              background: "#000",
+              color: "#fff",
+              fontSize: 10,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              fontWeight: 700,
+            }}
+          >
+            {cart.length}
+          </div>
+        )}
+
         <div className="mobile-icon">🛒</div>
         <span>Cart</span>
       </Link>
@@ -23,6 +52,7 @@ export default function MobileBottomBar() {
       <a
         href="https://wa.me/201060230817"
         target="_blank"
+        rel="noopener noreferrer"
         className="mobile-item"
       >
         <div className="mobile-icon">💬</div>
