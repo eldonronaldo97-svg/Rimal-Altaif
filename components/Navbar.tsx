86541a2541ba1b14d8691ea399d62b799a11de6f
abdallah <eldonronaldo97@gmail.com>
2026-06-11 07:52:21 +0300

    new navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,71 +1,57 @@
 "use client";
 
 import Link from "next/link";
-import { useCart } from "../lib/store";
 
-export default function MobileBottomBar() {
-  const cart = useCart((s) => s.cart);
-
+export default function Navbar() {
   return (
-    <div className="mobile-bar">
-      <Link href="/" className="mobile-item">
-        <div className="mobile-icon">🏠</div>
-        <span>Home</span>
-      </Link>
-
-      <Link href="/brands" className="mobile-item">
-        <div className="mobile-icon">🛍️</div>
-        <span>Brands</span>
-      </Link>
+    <header
+      style={{
+        position: "sticky",
+        top: 0,
+        zIndex: 999,
+        height: 70,
+        background: "#fff",
+        borderBottom: "1px solid #eee",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: "0 20px",
+      }}
+    >
+      <button
+        style={{
+          background: "none",
+          border: "none",
+          fontSize: 24,
+          cursor: "pointer",
+        }}
+      >
+        ☰
+      </button>
 
       <Link
-        href="/cart"
-        className="mobile-item"
-        style={{ position: "relative" }}
+        href="/"
+        style={{
+          textDecoration: "none",
+          color: "#000",
+          fontSize: 22,
+          fontWeight: 600,
+          letterSpacing: 1,
+        }}
       >
-        {cart.length > 0 && (
-          <div
-            style={{
-              position: "absolute",
-              top: -8,
-              right: -8,
-              width: 18,
-              height: 18,
-              borderRadius: "50%",
-              background: "#000",
-              color: "#fff",
-              fontSize: 10,
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              fontWeight: 700,
-            }}
-          >
-            {cart.length}
-          </div>
-        )}
-
-        <div className="mobile-icon">🛒</div>
-        <span>Cart</span>
+        Rimal Altaif
       </Link>
 
-      <a
-        href="https://wa.me/201060230817"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="mobile-item"
+      <button
+        style={{
+          background: "none",
+          border: "none",
+          fontSize: 22,
+          cursor: "pointer",
+        }}
       >
-        <div className="mobile-icon">💬</div>
-        <span>WhatsApp</span>
-      </a>
-
-      <a
-        href="tel:01060230817"
-        className="mobile-item"
-      >
-        <div className="mobile-icon">📞</div>
-        <span>Call</span>
-      </a>
-    </div>
+        🔍
+      </button>
+    </header>
   );
 }
