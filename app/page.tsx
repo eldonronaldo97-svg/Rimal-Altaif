9fafab9a218067a4b5b23724e6e84017692b1de7
abdallah <eldonronaldo97@gmail.com>
2026-06-12 18:19:54 +0300

    show in-stock products first on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -12,12 +12,19 @@ import {
   homeBestSellers,
 } from "../lib/products";
 
+function sortByStock(items: any[]) {
+  return [...items].sort((a, b) => {
+    if (a.stock === b.stock) return 0;
+    return a.stock ? -1 : 1;
+  });
+}
+
 export default function Home() {
   const bestSellers = homeBestSellers
-  .map((id) =>
-    products.find((p) => p.id === id)
-  )
-  .filter(Boolean);
+    .map((id) =>
+      products.find((p) => p.id === id)
+    )
+    .filter(Boolean);
 
   const latestProducts = [...products]
     .reverse()
@@ -36,38 +43,44 @@ export default function Home() {
 
       <ProductSlider
         title="Best Selling"
-        products={bestSellers}
+        products={sortByStock(bestSellers)}
       />
 
       <ProductSlider
         title="Latest Release"
-        products={latestProducts}
+        products={sortByStock(latestProducts)}
       />
 
       <ProductSlider
-  title="Men Perfumes"
-  products={products.filter(
-    (p) => p.category === "Men"
-  )}
-/>
+        title="Men Perfumes"
+        products={sortByStock(
+          products.filter(
+            (p) => p.category === "Men"
+          )
+        )}
+      />
 
-<ProductSlider
-  title="Women Perfumes"
-  products={products.filter(
-    (p) => p.category === "Women"
-  )}
-/>
+      <ProductSlider
+        title="Women Perfumes"
+        products={sortByStock(
+          products.filter(
+            (p) => p.category === "Women"
+          )
+        )}
+      />
 
-<ProductSlider
-  title="Unisex Perfumes"
-  products={products.filter(
-    (p) => p.category === "Unisex"
-  )}
-/>
+      <ProductSlider
+        title="Unisex Perfumes"
+        products={sortByStock(
+          products.filter(
+            (p) => p.category === "Unisex"
+          )
+        )}
+      />
 
       <ProductSlider
         title="All Products"
-        products={featuredProducts}
+        products={sortByStock(featuredProducts)}
       />
 
       <Footer />
